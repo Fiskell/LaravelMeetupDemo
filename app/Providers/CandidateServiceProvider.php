87d296e60a1b064b447112a57997b7b74e4b29c3
Fiskell <stephen.fiskell@gmail.com>
2016-01-03 20:07:15 -0800

    Add service provider for candidates

---
+++ app/Providers/CandidateServiceProvider.php
@@ -0,0 +1,31 @@
+<?php namespace App\Providers;
+
+use App\Examples\StrategyPattern\Candidates\Clinton;
+use App\Examples\StrategyPattern\Candidates\Sanders;
+use App\Examples\StrategyPattern\Candidates\Trump;
+use Illuminate\Support\ServiceProvider;
+
+class CandidateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO don't always want to bind like this, give more examples
+
+        $this->app->bind('TheDonald', function() {
+            return new Trump();
+        });
+
+        $this->app->bind('Hillary', function() {
+            return new Clinton();
+        });
+
+        $this->app->bind('Bernie', function() {
+            return new Sanders();
+        });
+    }
+}
