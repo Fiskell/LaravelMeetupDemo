447d75396ea124b2ee2b8feed61777b3a63f61ef
Fiskell <stephen.fiskell@gmail.com>
2016-01-04 23:26:15 -0800

    Add reunion artisan command

---
+++ app/Console/Commands/Reunion.php
@@ -0,0 +1,27 @@
+<?php namespace App\Console\Commands;
+
+use App\Examples\MethodInjection\KyloRen;
+use Illuminate\Console\Command;
+use Illuminate\Container\Container;
+
+class Reunion extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'demo:daddy-issues';
+
+    /**
+     * Execute the console command.
+     *
+     * @param Container $container
+     * @param KyloRen $kyloRen
+     * @return mixed
+     */
+    public function handle(Container $container, KyloRen $kyloRen)
+    {
+        $container->call([$kyloRen, 'talkToDad']);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Debate::class
+        Commands\Debate::class,
+        Commands\Reunion::class,
     ];
 
     /**
