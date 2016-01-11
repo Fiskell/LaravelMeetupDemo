220555ea2cfdd3332bb536b58e0783023dad1c4c
Fiskell <stephen.fiskell@gmail.com>
2016-01-10 20:09:29 -0800

    Fix constructor path issue

--- app/Http/Controllers/v1/Orders.php
+++ app/Http/Controllers/v1/Orders.php
@@ -1,10 +1,14 @@
 <?php namespace App\Http\Controllers\v1;
 
+use App\Examples\Stitch\Orders\v2\OrderInterface;
 use App\Http\Controllers\BaseEndpointController;
 
 class Orders extends BaseEndpointController
 {
-    public function __construct(App\Examples\Stitch\Orders\v1\OrderInterface $ordersRepository) {
+    /**
+     * @param \App\Examples\Stitch\Orders\v2\OrderInterface $ordersRepository
+     */
+    public function __construct(OrderInterface $ordersRepository) {
 
     }
 }
--- app/Http/Controllers/v2/Orders.php
+++ app/Http/Controllers/v2/Orders.php
@@ -1,10 +1,14 @@
 <?php namespace App\Http\Controllers\v2;
 
+use App\Examples\Stitch\Orders\v1\OrderInterface;
 use App\Http\Controllers\BaseEndpointController;
 
 class Orders extends BaseEndpointController
 {
-    public function __construct(App\Examples\Stitch\Orders\v1\OrderInterface $ordersRepository) {
+    /**
+     * @param \App\Examples\Stitch\Orders\v1\OrderInterface $ordersRepository
+     */
+    public function __construct(OrderInterface $ordersRepository) {
 
     }
 }
