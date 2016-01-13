48ed7d8b2e7c7c50ae98e7f36c877f9e5ae56db7
Fiskell <stephen.fiskell@gmail.com>
2016-01-12 18:25:57 -0800

    Remove method injection example

--- app/Examples/MethodInjection/Betrayal.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App\Examples\MethodInjection;
-
-class Betrayal {
-
-    public function doSomethingMean() {
-        echo "Stab dad through the heart with a lightsaber\n";
-    }
-}
